e7272fe85bc4c88314afb807eddff308d53a4df6
Jigyasa Jain <jigyasa1124.becse24@chitkara.edu.in>
2025-02-08 21:50:02 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -5,6 +5,9 @@ int main(){
     if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'){
         printf("Vowel");
     }
+    else if(a>=0 && a<=9){
+        printf("Digit");
+    }
     else{
         printf("Consonant");
     }
