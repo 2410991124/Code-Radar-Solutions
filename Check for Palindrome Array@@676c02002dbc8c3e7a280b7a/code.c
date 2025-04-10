fd8d87c2bfbbeba84b047a83b911307ebb7625ac
Jigyasa Jain <jainajigyasa@gmail.com>
2025-04-11 02:38:23 +0530

    Solution to Check for Palindrome Array

--- Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
+++ Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
@@ -1,23 +1,50 @@
+// #include <stdio.h>
+// int main(){
+//     int N;
+//     scanf("%d", &N);
+//     int a[N];
+//     for(int i=0; i<N; i++){
+//         scanf("%d", &N);
+//     }
+//     int p=1;
+//     for(int i=0, j=N-1; i<j; i++, j--){
+//         if(a[i]!=a[j]){
+//             p=0;
+//             break;
+//         }
+//     }
+//     if(p==1){
+//         printf("YES");
+//     }
+//     else{
+//         printf("NO");
+//     }
+//     return 0;
+// }
+
 #include <stdio.h>
-int main(){
+int main() {
     int N;
     scanf("%d", &N);
-    int a[N];
-    for(int i=0; i<N; i++){
-        scanf("%d", &N);
+    int a[N]; 
+    for (int i = 0; i < N; i++) {
+        // if (scanf("%d", &a[i]) != 1) {
+        //     printf("Invalid input. Please enter integers only.\n");
+        //     return 1; 
+        // }
+        scanf("%d", &a[i]);
     }
-    int p=1;
-    for(int i=0, j=N-1; i<j; i++, j--){
-        if(a[i]!=a[j]){
-            p=0;
+    int isPalindrome = 1; 
+    for (int i = 0, j = N - 1; i < j; i++, j--) {
+        if (a[i] != a[j]) {
+            isPalindrome = 0; 
             break;
         }
     }
-    if(p==1){
-        printf("YES");
-    }
-    else{
-        printf("NO");
+    if (isPalindrome == 1) {
+        printf("YES\n");
+    } else {
+        printf("NO\n");
     }
     return 0;
 }
